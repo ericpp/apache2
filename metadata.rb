e4c0e5c8defb0f5bccb9ad95a313cd600ea6b5e7
Joshua Buysse <buysse@umn.edu>
2014-06-23 21:30:05 -0500

    update metadata to include name attribute.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
+name              "apache2"
 description       "Installs and configures all aspects of apache2 using Debian style symlinks with helper definitions"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.1.16"
